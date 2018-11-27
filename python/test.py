6948ded41438b2e2eff1dee360830fe6e75a7732
oyjx <1005281342@qq.com>
2018-11-27 20:43:12 +0800

    update

---
+++ python/test.py
@@ -0,0 +1,22 @@
+def total(a=5, *args, **kwargs):
+
+    """
+
+    :param a:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+
+    print(a)
+
+    for x in args:
+
+        print(x)
+
+    for c, d in kwargs.items():
+        print(c, d)
+
+
+print(total(100, 1, 2, 3, Jack=1234, aa=4321))
+print(total.__doc__)
